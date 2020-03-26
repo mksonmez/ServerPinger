e5f36bd3a0db4c33335ff6a66c56f7523d0563b5
Mehmet Sonmez <gate3510@gmail.com>
2020-03-26 18:37:00 -0400

    inital commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import subprocess
+import csv
+
+def ping(target_ip):
+    p = subprocess.Popen('ping ' + target_ip, 
+				    	stdout=subprocess.PIPE, 
+				    	stderr=subprocess.PIPE)
+
+    onGoing = 'server-online'
+
+# check console output        
+    for line in p.stdout:
+        output = line.rstrip().decode('UTF-8')
+        if (output.endswith('unreachable.')) :
+            onGoing = 'server-unreacheable'
+            break
+        elif (output.startswith('Ping request could not find host')) :
+            onGoing = 'server-host-not-found'
+            break
+        if (output.startswith('Request timed out.')) :
+            onGoing = 'server-offline'
+            break
+        else:
+            print(target_ip)
+    
+    return onGoing
+
+# write to file on or off
+def on_or_off(target_ip):
+    statusOfPing = ping(target_ip)
+    if (statusOfPing == 'server-host-not-found') :
+        writeToFile('server-not-found.csv', target_ip)
+    elif (statusOfPing == 'server-unreacheable') :
+       writeToFile('server-unreachable.csv', target_ip)
+    elif (statusOfPing == 'server-offline') :
+       writeToFile('server-offline.csv', target_ip)
+    elif (statusOfPing == 'server-online') :
+        writeToFile('server-online.csv', target_ip)
+    else:
+        print("error on on_or_off")
+
+def writeToFile(filename, data) :
+    with open(filename, 'a') as output:
+        output.write('header' + '\n' + data + '\n')
+
+# source if IPs/server
+file = open('list_of_ips.txt')
+
+try:
+    reader = csv.reader(file)
+    
+    for item in reader:
+        on_or_off(item[0].strip())
+finally:
+    file.close()
